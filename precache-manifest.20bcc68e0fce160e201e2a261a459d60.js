f67d8c32ee55e4a3811491d470adbd4a12d27df4
Aizat Faiz <aizat.faiz@gmail.com>
2019-11-06 11:31:10 +0800

    Updates

---
+++ precache-manifest.20bcc68e0fce160e201e2a261a459d60.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df19ded1dce211d6fd116786aafd0720",
+    "url": "/maps/index.html"
+  },
+  {
+    "revision": "13843f5a9736cd6a8b5a",
+    "url": "/maps/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "68e80019c9d68735954a",
+    "url": "/maps/static/js/2.5ba8e58f.chunk.js"
+  },
+  {
+    "revision": "13843f5a9736cd6a8b5a",
+    "url": "/maps/static/js/main.7b1a4027.chunk.js"
+  },
+  {
+    "revision": "4e8f18a1cbbe9a5e0287",
+    "url": "/maps/static/js/runtime-main.07546238.js"
+  }
+]);
